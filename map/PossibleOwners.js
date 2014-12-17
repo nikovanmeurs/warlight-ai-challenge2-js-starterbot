1e752ec5481eb24132fb7fd9af63a2397ad9ff18
Niko van Meurs <nikovanmeurs@gmail.com>
2014-12-17 15:53:16 +0100

    Initial commit

---
+++ map/PossibleOwners.js
@@ -0,0 +1,8 @@
+/**
+ * Convenience object containing references to the possible region owners
+ */
+PossibleOwners = {
+    NEUTRAL: 'neutral',
+    OPPONENT: 'opponent',
+    PLAYER: 'player',
+};
---
+++ map/Region.js
@@ -0,0 +1,24 @@
+require('./PossibleOwners.js');
+
+/**
+ * Region class
+ * Initializes with an id and a reference to the super region
+ * @param int id
+ * @param SuperRegion superRegion
+ */
+Region = function (id, superRegion) {
+
+    if (false === ( this instanceof Region)) {
+        return new Region(arguments);
+    }
+
+    this.id = id;
+    this.superRegion = superRegion;
+    this.owner = PossibleOwners.NEUTRAL;
+    this.neighbors = [];
+    this.troopCount = 2;
+    this.isOnEmpireBorder = false;
+    this.isOnSuperRegionBorder = false;
+}
+
+module.exports = Region;
